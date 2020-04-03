2d9c82084e9ec0cb13ad4f9bd78656b00d48cf59
Aeroblast <15964528+Aeroblast@users.noreply.github.com>
2020-04-03 10:17:20 +0800

    Update main.js

--- main.js
+++ main.js
@@ -121,7 +121,7 @@ function Tags2HTML(a) {
 }
 function Index2HTML(a) {
     let s = "";
-    if (a.length > 1)
+    if (a.length > 2)
         s += "Title:<span class='title'>" + a[2] + "</span><br>Tags:";
     for (let i = 3; i < a.length; i++)if (a[i]) s += "<div class='tag' onclick='ClickTag(this,event);'>" + a[i] + "</div>";
     return s;
