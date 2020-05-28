c92ea0ddd03b2c4c46db995167c4172ac675ec85
Aeroblast <15964528+Aeroblast@users.noreply.github.com>
2020-05-28 14:08:12 +0800

    highlight

--- main.js
+++ main.js
@@ -332,16 +332,21 @@ function RenderContent() {
     txtcontent.forEach(ele => {
         var c = ele.replace('\r', '');
         c = EncodeAtxt(c);
+        if(encodeState=="code"){code+=c+"\n";return;}
         var cla = "";
         if (c.length == 0) c = "<br>";
         if (c[0] == "「" || c[0] == "『" || c[0] == "（") cla += " drawout";
-        if (c.startsWith("<div") || c.startsWith("</div"))
+        if (c.startsWith("<div") || c.startsWith("</div")||c.startsWith("</code>"))
             code += c;
         else
             code += "<p id='line" + line + "' class='" + cla + "'>" + c + "</p>";
         line++;
     });
     main.innerHTML = code;
+    hljs.configure({useBR: true});
+    document.querySelectorAll('code').forEach((block) => {
+        hljs.highlightBlock(block);
+      });
     var ua = navigator.userAgent;
     var iPad = ua.match(/(iPad).*OS\s([\d_]+)/),
         iPhone = !iPad && ua.match(/(iPhone\sOS)\s([\d_]+)/),
@@ -374,7 +379,7 @@ function EncodeAtxt(c) {
         [/\[note\]/, ""],
         [/\[note=(.*?)\]/, ""],
         [/\[code\]/, "<code>"],
-        [/\[code=(.*?)\]/, "<code lang='$1'>"],
+        [/\[code=(.*?)\]/, "<code class='lang-$1'>"],
         //[/\[\/code\]/, "</code>"],//processed proviously
         [/\[img\](.*?)\[\/img\]/, "<img class='aimg' src='Images/$1'>"],
         [/\[img=(.*?),(.*?)\](.*?)\[\/img\]/, "<img class='aimg' style='width:$1;height:$2' src='Images/$3'>"],
