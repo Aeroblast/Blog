de7d027a31c5b4cd9eb90568c5f52444f9cfa71d
Aeroblast <15964528+Aeroblast@users.noreply.github.com>
2021-01-31 23:53:42 +0800

    修复bug
    js 设置月份 31日设置小月 会跳

--- util.js
+++ util.js
@@ -1,4 +1,4 @@
-const sharedDatetimeFormat=new Intl.DateTimeFormat('zh-CN-u-hc-h24', { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' });
+const sharedDatetimeFormat = new Intl.DateTimeFormat('zh-CN-u-hc-h24', { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' });
 function GetQueryString(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
     var r = window.location.search.substr(1).match(reg);
@@ -8,7 +8,7 @@ function isNum(char) {
     return char >= '0' && char <= '9';
 }
 function TryGetDate(str) {
-    let date = new Date();
+    let year = 0, month = 0, day = 0, hours = 0, minutes = 0, seconds = 0;
     let p = 0;
     let d = 0;
     let temp = "";
@@ -16,17 +16,17 @@ function TryGetDate(str) {
         if (isNum(str[p])) {
             temp += str[p];
             d++;
-            if (d == 4) { date.setFullYear(parseInt(temp)); temp = ""; }
-            if (d == 6) { date.setMonth(parseInt(temp) - 1); temp = ""; }
-            if (d == 8) { date.setDate(parseInt(temp)); temp = ""; }
-            if (d == 10) { date.setHours(parseInt(temp)); temp = ""; }
-            if (d == 12) { date.setMinutes(parseInt(temp)); temp = ""; }
-            if (d == 14) { date.setSeconds(parseInt(temp)); temp = ""; }
+            if (d == 4) { year = parseInt(temp); temp = ""; }
+            if (d == 6) { month = parseInt(temp) - 1; temp = ""; }
+            if (d == 8) { day = parseInt(temp); temp = ""; }
+            if (d == 10) { hours = parseInt(temp); temp = ""; }
+            if (d == 12) { minutes = parseInt(temp); temp = ""; }
+            if (d == 14) { seconds = parseInt(temp); temp = ""; }
         }
         p++;
     }
     if (d < 8) return null;
-    return date;
+    return new Date(year, month, day, hours, minutes, seconds, 0);
 }
 function ReplaceEntity(c) {
     return c.replace('<', '&lt;').replace('>', '&rt;');
