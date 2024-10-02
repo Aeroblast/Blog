8f76c63efa6aa93bcd071ab729f51e62f1e02844
Aeroblast <15964528+Aeroblast@users.noreply.github.com>
2024-10-02 09:29:42 +0900

    Update TocNestedList.vue

--- vue/src/components/TocNestedList.vue
+++ vue/src/components/TocNestedList.vue
@@ -1,6 +1,6 @@
 <template>
     <div :data-level="data.level">
-        <div class="text" @click.once="$emit('ClickItem', data)">{{ data.text }}</div>
+        <div class="text" @click.stop="$emit('ClickItem', data)">{{ data.text }}</div>
         <ol v-if="data.children.length > 0">
             <li v-for="item in data.children">
                 <TocNestedList :data="item" @ClickItem="$emit('ClickItem', $event)"></TocNestedList>
